e7941f0ea52286e5fb4f7950fe4dd245f82ea81a
PaulBouchard <paul.bouchard.pro@gmail.com>
2022-12-24 01:05:39 +0100

    Q2 fini

--- deblab.c
+++ deblab.c
@@ -3,25 +3,33 @@
 #include "labyrinthAPI.h"
 #include "clientAPI.h"
 
+void demande_coup_joueur(t_move * mouvement){
+    int intermediaire_typenum;
+    printf("Entrez un chiffre entre 0 et 3 indiquant par quel côté la piece sera insérée (0=O,1=E,2=N,S=3)\n");
+    scanf("%d",&intermediaire_typenum);
+    mouvement->insert = intermediaire_typenum;
+        
+    printf("Entrez le numéro de la ligne ou de la colonne dans laquelle ira la pièce\n");
+    scanf("%d",&mouvement->number);
+        
+    printf("Entrez un chiffre entre 0 et 3 indiquant la rotation appliquée à la pièce en sens horaire\n");
+    scanf("%d",&mouvement->rotation);
+
+    printf("Entrez les coordonnées de la case où vous voulez aller (x y)\n");
+    scanf("%d %d",&mouvement->x,&mouvement->y);
+}
+
 int main(void){
-    int numero_joueur_depart;
+    int numero_joueur_depart,num_mouv_joueur,num_mouv_bot = 0;
     int tailleX,tailleY;
     int case_N,case_E,case_S,case_O,case_I;
     char nom_jeu[50];
-    int intermediaire_typenum;
+    t_move mouv_joueur,mouv_bot;
 
     connectToServer("172.105.76.204",1234,"DONTMOVE");
-    waitForLabyrinth("TRAINING DONTMOVE key=1000",nom_jeu,&tailleX,&tailleY);
+    waitForLabyrinth("TRAINING DONTMOVE timeout=1000",nom_jeu,&tailleX,&tailleY);
     int *lab = malloc(5*tailleX*tailleY*sizeof(int));
     numero_joueur_depart = getLabyrinth(lab,&case_N,&case_E,&case_S,&case_O,&case_I);
-    
-    printf("\n%d\n",numero_joueur_depart);
-    
-    t_move mouv_joueur;
-    int num_mouv_joueur = 0;
-    
-    t_move mouv_bot;
-    int num_mouv_bot = 0;
     
     if (numero_joueur_depart == 1){
         getMove(&mouv_bot);
@@ -32,26 +40,12 @@ int main(void){
     }
 
     while (1){
-        printf("Entrez un chiffre entre 0 et 3 indiquant par quel côté la piece sera insérée (0=O,1=E,2=N,S=3)\n");
-        scanf("%d",&intermediaire_typenum);
-        mouv_joueur.insert = intermediaire_typenum;
-        
-        printf("Entrez le numéro de la ligne ou de la colonne dans laquelle ira la pièce\n");
-        scanf("%d",&(mouv_joueur.number));
-        
-        printf("Entrez un chiffre entre 0 et 3 indiquant la rotation appliquée à la pièce en sens horaire\n");
-        scanf("%d",&(mouv_joueur.rotation));
-        
-        printf("Entrez les coordonnées de la case où vous voulez aller (x y)\n");
-        scanf("%d %d",&(mouv_joueur.x),&(mouv_joueur.y));
+        demande_coup_joueur(&mouv_joueur);
 
         num_mouv_joueur = sendMove(&mouv_joueur);
-        printLabyrinth();
-        //getLabyrinth(lab,&(mouv_joueur.tileN),&(mouv_joueur.tileE),&(mouv_joueur.tileS),&(mouv_joueur.tileW),&(mouv_joueur.tileItem));
 
         num_mouv_bot = getMove(&mouv_bot);
         printLabyrinth();
-        //getLabyrinth(lab,&case_N,&case_E,&case_S,&case_O,&case_I);
 
         if (num_mouv_joueur == 1){
             printf("Vous avez gagné");
@@ -75,5 +69,6 @@ int main(void){
             return 0;
         }   
     }
+    closeConnection();
     return 0;
 }
